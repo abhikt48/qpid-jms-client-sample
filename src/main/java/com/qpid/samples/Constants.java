96ece346363c81854e149fe7a08b0b2362941fbc
Abhishek Kumar <abhikt48@gmail.com>
2021-01-18 19:45:42 +0000

    Create Constants.java

---
+++ src/main/java/com/qpid/samples/Constants.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package com.qpid.samples;
+
+public interface Constants {
+
+    public static final String PUBLISHER_QUEUE_NAME = "**";
+    public static final String RECEIVER_QUEUE_NAME = "****";
+    public static final String SBUS_NAME = "***";
+    public static final String USERNAME = "***";
+    public static final String PASSWORD = "****";
+    public static final String QPID_CONNECTION_FACTORY_CLASS = "org.apache.qpid.jms.jndi.JmsInitialContextFactory";
+
+
+}

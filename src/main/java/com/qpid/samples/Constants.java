41c6ca3d42ba1b55dd5cf0daf2871b68e643a8bf
Abhishek Kumar <abhikt48@gmail.com>
2021-01-19 13:12:57 +0000

    Added comment

--- src/main/java/com/qpid/samples/Constants.java
+++ src/main/java/com/qpid/samples/Constants.java
@@ -7,7 +7,7 @@ public interface Constants {
 
     public static final String PUBLISHER_QUEUE_NAME = "**";
     public static final String RECEIVER_QUEUE_NAME = "****";
-    public static final String SBUS_NAME = "***";
+    public static final String SBUS_NAME = "***";  // Only Service BUS name without extension ".servicebus.windows.net"
     public static final String USERNAME = "***";
     public static final String PASSWORD = "****";
     public static final String QPID_CONNECTION_FACTORY_CLASS = "org.apache.qpid.jms.jndi.JmsInitialContextFactory";
